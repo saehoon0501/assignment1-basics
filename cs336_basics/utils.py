07b8e82dbd28a0c3d2646979a8b46c7d6d0a49b1
saehoon0501 <saehoon0501@gmail.com>
2025-07-29 11:06:22 +0900

    data loader init

---
+++ cs336_basics/utils.py
@@ -0,0 +1,37 @@
+import torch
+import numpy.typing as npt
+from einops import rearrange
+
+def get_batch(
+    dataset: npt.NDArray, batch_size: int, context_length: int, device: str
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """
+    Given a dataset (a 1D numpy array of integers) and a desired batch size and
+    context length, sample language modeling input sequences and their corresponding
+    labels from the dataset.
+
+    Args:
+        dataset (np.array): 1D numpy array of integer token IDs in the dataset.
+        batch_size (int): Desired batch size to sample.
+        context_length (int): Desired context length of each sampled example.
+        device (str): PyTorch device string (e.g., 'cpu' or 'cuda:0') indicating the device
+            to place the sampled input sequences and labels on.
+
+    Returns:
+        Tuple of torch.LongTensors of shape (batch_size, context_length). The first tuple item
+        is the sampled input sequences, and the second tuple item is the corresponding
+        language modeling labels.
+    """
+    # convert numpy 1d array into 1d long tensor
+    data = torch.from_numpy(dataset).long()
+
+    # Randomly sample starting indices for our batch
+    ix = torch.randint(len(data) - context_length, (batch_size,))
+
+    # Create the input and target sequences
+    x = torch.stack([data[i : i + context_length] for i in ix])
+    y = torch.stack([data[i + 1 : i + context_length + 1] for i in ix])    
+    
+    # Move tensors to the correct device
+    x, y = x.to(device), y.to(device)
+    return x, y
